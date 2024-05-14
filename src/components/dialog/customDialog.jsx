28755b3b39ce3e8f660ba08fb5ab445374c42a73
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-14 22:08:29 +0600

    Reusable Modal

---
+++ src/components/dialog/customDialog.jsx
@@ -0,0 +1,58 @@
+import { Button } from '../buttons/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from './dialog';
+import { Input } from '../../../components/ui/input';
+import { Label } from '../../../components/ui/label';
+
+export const CustomDialog = ({
+  triggerText,
+  triggerTextVariant,
+  triggerTextStyle,
+  handleOnSubmit,
+  title,
+  description,
+  children,
+  dialogCloseText,
+  dialogCloseAction,
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {triggerText && (
+          <Button
+            variant={triggerTextVariant}
+            className={triggerTextStyle}
+            onClick={handleOnSubmit}
+          >
+            {triggerText}
+          </Button>
+        )}
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          {title && <DialogTitle>{title}</DialogTitle>}
+          {description && (
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          )}
+        </DialogHeader>
+        {children}
+        <DialogFooter>
+          <DialogClose className="w-full">
+            {dialogCloseText && <Button onClick={dialogCloseAction}>{dialogCloseText}</Button>}
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
