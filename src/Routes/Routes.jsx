0b2dfc6ab6e899e9249cd8859649cfc4bfdc650a
Amdadul Islam <amdadul.islam.rayhan@gmail.com>
2024-05-16 14:41:52 +0600

    Removed redundant codes from Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,32 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Auth from '../modules/User/Pages/Auth/Auth';
 import UpdateProfile from '../modules/User/Pages/UpdateProfile/UpdateProfile';
-import Sidebar from '../components/sidebar/sidebar';
 import { useAuth } from '../modules/User/context/AuthContext';
-import Signin from '../modules/User/Pages/SignIn/SignIn';
-
-// const routes = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: (
-//       <Link to="/auth" className="inline-block px-4 py-2 m-16 border rounded-lg">
-//         SignIn
-//       </Link>
-//     ),
-//   },
-//   {
-//     path: '/auth',
-//     element: <Auth />,
-//   },
-//   {
-//     path: '/update',
-//     element: <UpdateProfile />,
-//   },
-// ]);
-
-// import React from 'react';
-// import { useAuth } from '../modules/User/context/AuthContext';
-// import Signin from '../modules/User/Pages/SignIn/SignIn';
 
 const Routers = () => {
   const { user } = useAuth();
