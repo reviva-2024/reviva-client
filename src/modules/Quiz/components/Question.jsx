701b1e04e0fffe0af520f439c5000b3ee0eae32f
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-05-17 00:13:39 +0600

    Changed font weight

--- src/modules/Quiz/components/Question.jsx
+++ src/modules/Quiz/components/Question.jsx
@@ -9,7 +9,7 @@ const Question = ({ question, options, selectedOption, onOptionSelect, index }) 
     return (
       <label
         htmlFor={id}
-        className={`px-4 w-full inline-block py-2 rounded-lg ${selectedOption === id ? 'bg-primary bg-opacity-80  font-semibold text-white ' : ''}`}
+        className={`px-2 w-full inline-block py-2 rounded-lg ${selectedOption === id ? 'bg-primary bg-opacity-80  font-semibold text-white ' : ''}`}
       >
         <input
           type="radio"
@@ -26,8 +26,8 @@ const Question = ({ question, options, selectedOption, onOptionSelect, index }) 
 
   return (
     <div className="w-full bg-[#FEF7E7] rounded-lg ">
-      <p className="px-4 py-2 mb-4 rounded-t-lg bg-secondary">
-        <span className="mr-2">{index + 1}.</span>
+      <p className="px-4 py-2 mb-4 font-semibold rounded-t-lg bg-secondary">
+        <span className="mr-1">{index + 1}.</span>
         <span>{question}</span>
       </p>
       <ul className="grid grid-cols-1 px-4 pb-4 gap-y-3">
