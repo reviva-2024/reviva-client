5134b53cbdd5b320d8ba1a6b590d8bb687850ee4
Amdadul Islam <amdadul.islam.rayhan@gmail.com>
2024-05-16 15:14:03 +0600

    Fixed nested routing issue

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Auth from '../modules/User/Pages/Auth/Auth';
 import UpdateProfile from '../modules/User/Pages/UpdateProfile/UpdateProfile';
 import { useAuth } from '../modules/User/context/AuthContext';
@@ -12,9 +12,18 @@ const Routers = () => {
       <Route
         exact
         path="/"
-        element={user ? <div className="text-black">Home</div> : <Navigate to={'/auth'} />}
+        element={
+          user ? (
+            <div className="flex w-full gap-6">
+              <aside>Sidebar{/* Add sidebar component here after creating the homepage*/}</aside>
+              <Outlet />
+            </div>
+          ) : (
+            <Navigate to={'/auth'} />
+          )
+        }
       >
-        <Route path="/update" element={<UpdateProfile />} />
+        <Route exact path="auth/update" element={<UpdateProfile />} />
       </Route>
     </Routes>
   );
