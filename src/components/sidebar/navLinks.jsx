2d1eede493bead87fc3d56856d0b55459002cade
MisbahKhan0009 <misbah.khan@northsouth.edu>
2024-05-18 20:55:42 +0600

    Sidebar Added

---
+++ src/components/sidebar/navLinks.jsx
@@ -0,0 +1,32 @@
+import { Settings, User2, Brain, ListVideo, HelpCircle, LucideLogOut } from 'lucide-react';
+
+export const firstUlItems = [
+  {
+    icon: <User2 size={22} />,
+    text: 'Profile',
+  },
+  {
+    icon: <Brain size={22} />,
+    text: 'Quiz',
+  },
+  {
+    icon: <ListVideo size={22} />,
+    text: 'Profile',
+  },
+];
+
+export const secondUlItems = [
+  {
+    icon: <HelpCircle size={22} />,
+    text: 'Help',
+  },
+  {
+    icon: <Settings size={22} />,
+    text: 'Settings',
+  },
+  {
+    icon: <LucideLogOut size={22} />,
+    text: 'Log out',
+  },
+];
+
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,14 +1,6 @@
-import {
-  Settings,
-  Menu,
-  ArrowLeft,
-  User2,
-  Brain,
-  ListVideo,
-  HelpCircle,
-  LucideLogOut,
-} from 'lucide-react';
+import { Menu, ArrowLeft } from 'lucide-react';
 import React, { useState } from 'react';
+import { firstUlItems, secondUlItems } from './navLinks';
 
 export default function Sidebar() {
   const [expanded, setExpanded] = useState(false);
@@ -17,60 +9,34 @@ export default function Sidebar() {
     setExpanded((prevExpanded) => !prevExpanded);
   };
 
-  const firstUlItems = [
-    {
-      icon: <User2 size={20} />,
-      text: 'Profile',
-    },
-    {
-      icon: <Brain size={20} />,
-      text: 'Quiz',
-    },
-    {
-      icon: <ListVideo size={20} />,
-      text: 'Profile',
-    },
-  ];
-
-  const secondUlItems = [
-    {
-      icon: <HelpCircle size={20} />,
-      text: 'Help',
-    },
-    {
-      icon: <Settings size={20} />,
-      text: 'Settings',
-    },
-    {
-      icon: <LucideLogOut size={20} />,
-      text: 'Log out',
-    },
-  ];
-
   return (
-    <aside className={`h-screen ${expanded ? 'w-72' : 'w-20'}`}>
-      <div className="p-4 pb-2 flex   justify-end">
+    <aside
+      className={`fixed top-0 bottom-0 ${expanded ? 'w-72' : 'w-20'} bg-white rounded-xl shadow-xl z-50`}
+    >
+      <div className="p-4 pb-2 flex justify-end">
         <button
           onClick={toggleExpanded}
-          className={`p-1.5  rounded-lg ${!expanded ? 'mx-auto' : ''}`}
+          className={`p-1.5 rounded-lg ${!expanded ? 'mx-auto' : ''}`}
         >
-          {!expanded ? <Menu /> : <ArrowLeft className="border rounded text-primary" />}
+          {!expanded ? (
+            <Menu />
+          ) : (
+            <ArrowLeft className="border border-neutral-400 rounded text-primary" />
+          )}
         </button>
       </div>
-      <nav className="h-full inline-flex flex-col items-center rounded-xl shadow-xl">
+      <nav className="h-full inline-flex flex-col items-center">
         <img
           src={`${expanded ? 'https://i.ibb.co/8MMgCSv/REVIVA-LOGO.png' : 'https://i.ibb.co/GPG0Q8j/icon-1.png'}`}
-          className={`overflow-hidden  transition-all  ${expanded ? 'border-b w-36 ps-0 me-10' : 'border-0 w-5'}  py-6`}
+          className={`overflow-hidden transition-all ${expanded ? 'border-b w-36 ps-0 me-10' : 'border-0 w-5'} py-6`}
           alt=""
         />
-        <div
-          className={`flex flex-col h-full text-neutral-400 justify-between w-full items-center  `}
-        >
+        <div className="flex flex-col h-full text-neutral-400 justify-between w-full items-center">
           <ul className="flex-1 px-3">
             {firstUlItems.map((item, index) => (
               <li
                 key={index}
-                className="relative flex items-center py-3 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+                className="relative flex items-center py-3 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-neutral-400"
               >
                 {item.icon}
                 <span
@@ -89,7 +55,6 @@ export default function Sidebar() {
                 className="relative flex items-center py-3 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
               >
                 {item.icon}
-
                 <span
                   className={`overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0'}`}
                 >
