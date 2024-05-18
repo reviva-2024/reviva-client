5ea0f811d6e70a6ad393aa22f4a675e74dac1136
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-05-19 00:23:05 +0600

    Changed the skeleton color to gray

--- src/modules/Quiz/components/QuestionSkeleton.jsx
+++ src/modules/Quiz/components/QuestionSkeleton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const QuestionSkeleton = () => {
   return (
-    <div className="w-full bg-[#FEF7E7] rounded-lg animate-pulse h-[268px]">
-      <div className="h-12 mb-4 rounded-t-lg bg-secondary" />
+    <div className="w-full bg-neutral-100 rounded-lg animate-pulse h-[268px]">
+      <div className="h-12 mb-4 rounded-t-lg bg-neutral-200" />
     </div>
   );
 };
