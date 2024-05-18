029442df1dc7523e9b8ed9ab11f1aa62a0ddbed9
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-05-18 13:55:00 +0600

    Added loading skeleton to Question component to indicate data fetching state

--- src/modules/Quiz/api/quizApi.js
+++ src/modules/Quiz/api/quizApi.js
@@ -2,13 +2,9 @@ import { authURL} from "../../../api/intances";
 import { Style, logs } from "../../../utils/logs";
 import { trycatch } from "../../../utils/trycatch";
 
-
-
 export const getAllQuizesApi = async (token) => {
-
     
   logs("API Call: getAllQuizesApi", [], Style.api);
-
 
   const [getAllQuizesRes, getAllQuizesErr] = await trycatch(authURL(token).get("/quiz/getQuizes"));
 
--- src/modules/Quiz/pages/Quiz.jsx
+++ src/modules/Quiz/pages/Quiz.jsx
@@ -4,9 +4,11 @@ import { getAllQuizesApi } from '../api/quizApi';
 import { toast } from 'sonner';
 import { useAuth } from '../../User/context/AuthContext';
 import { Style, logs } from '../../../utils/logs';
+import QuestionSkeleton from '../components/QuestionSkeleton';
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [selectedOptions, setSelectedOptions] = useState(new Array(questions.length).fill(''));
   const [showResult, setShowResult] = useState(false);
   const { user } = useAuth();
@@ -30,7 +32,7 @@ const Quiz = () => {
   };
 
   useEffect(() => {
-    // setIsLoading(true);
+    setLoading(true);
 
     const getAllPurchaseReport = async () => {
       const response = await getAllQuizesApi(user.token);
@@ -38,8 +40,9 @@ const Quiz = () => {
 
       if (response.data.success) {
         setQuestions(response.data.data);
+        setLoading(false);
       } else {
-        // setIsLoading(false);
+        setLoading(false);
         toast.error(response.data.message);
       }
     };
@@ -48,16 +51,18 @@ const Quiz = () => {
 
   return (
     <div className="grid w-full max-h-screen gap-5 p-5 overflow-y-auto">
-      {questions.map(({ _id, question, options }, index) => (
-        <Question
-          key={_id}
-          question={question}
-          options={options}
-          index={index}
-          selectedOption={selectedOptions[index]}
-          onOptionSelect={(option) => handleOptionSelect(index, option)}
-        />
-      ))}
+      {loading
+        ? Array.from({ length: 4 }).map((_, idx) => <QuestionSkeleton key={idx} />)
+        : questions.map(({ _id, question, options }, index) => (
+            <Question
+              key={_id}
+              question={question}
+              options={options}
+              index={index}
+              selectedOption={selectedOptions[index]}
+              onOptionSelect={(option) => handleOptionSelect(index, option)}
+            />
+          ))}
       <button onClick={() => setShowResult(!showResult)}>Show Result</button>
       {showResult && <p>Score: {calculateScore()}</p>}
     </div>
