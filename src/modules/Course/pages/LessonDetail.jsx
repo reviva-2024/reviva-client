be9f93f16ee3c33a20d809c11e4a47c5eef6b421
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-21 00:28:21 +0600

    Maked iframer responsive

--- src/modules/Course/pages/LessonDetail.jsx
+++ src/modules/Course/pages/LessonDetail.jsx
@@ -6,25 +6,28 @@ const LessonDetail = () => {
   const { state: lessonDetail } = useLocation();
   const { lessons, courseTitle } = lessonDetail;
   const [currentLesson, setCurrentLesson] = useState(lessonDetail.currentLesson);
-  const restLessons = lessons.filter((lesson) => lesson.videoLink != currentLesson.videoLink);
+  const restLessons = lessons.filter((lesson) => lesson.videoLink !== currentLesson.videoLink);
   const goToPreviousPage = () => {
     window.history.go(-1);
   };
+
   return (
     <section className="w-full p-4 h-full">
       <h1 className="text-xl flex gap-3 items-center text-white bg-primary p-3 mb-3 rounded-md font-semibold">
-        <button className="bg-neutral-50/10 px-2" onClick={goToPreviousPage} to={'/courseLesson'}>
-          <Undo2 />{' '}
-        </button>{' '}
+        <button className="bg-neutral-50/10 px-2" onClick={goToPreviousPage}>
+          <Undo2 />
+        </button>
         Lesson {currentLesson.lessonNo} : {currentLesson.title}
       </h1>
       <div className="w-full rounded-md">
-        <iframe
-          className="w-full h-[700px]"
-          allowFullScreen
-          src={currentLesson.videoLink}
-          title="W3Schools Free Online Web Tutorials"
-        ></iframe>
+        <div className="video-wrapper lg:h-[50%]">
+          <iframe
+            className="video-iframe "
+            allowFullScreen
+            src={currentLesson.videoLink}
+            title="W3Schools Free Online Web Tutorials"
+          ></iframe>
+        </div>
         <div className="text-xl flex justify-between text-white bg-primary p-3 mb-3 rounded-b-md font-semibold">
           <h1>{courseTitle}</h1>
           <h1>
