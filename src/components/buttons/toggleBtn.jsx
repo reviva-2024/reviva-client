d08b4e95a4e356cbb897fedff5ebbdfa069d4a79
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-05-21 22:41:09 +0600

    Created re-usable toggle btn

---
+++ src/components/buttons/toggleBtn.jsx
@@ -0,0 +1,21 @@
+// Reusable Toggle Button Component
+const ToggleBtn = ({ isChecked, onToggle, labels, loading }) => {
+  return (
+    <button
+      className={`flex items-center p-1 w-12 h-7 rounded-full cursor-pointer bg-primary ${loading && 'bg-primary'}`}
+      onClick={onToggle}
+    >
+      {!loading && (
+        <p
+          className={`w-1/2 h-full flex items-center  justify-center bg-primary-light text-neutral-900  dark:bg-green-300   rounded-full transform transition-transform text-[10px] uppercase  font-bold  ${
+            isChecked ? 'translate-x-5' : 'translate-x-0'
+          }`}
+        >
+          <span className="m-1">{isChecked ? labels[1] : labels[0]}</span>
+        </p>
+      )}
+    </button>
+  );
+};
+
+export default ToggleBtn;
